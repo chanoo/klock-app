8125676f64073827cf5e6ce924508fe8435f5db6
“chanoo” <x64core@gmail.com>
2023-03-21 12:32:51 +0900

    앱 이름 변경 및 macos 초기 윈도우 화면 크기 조절

--- macos/Runner/MainFlutterWindow.swift
+++ macos/Runner/MainFlutterWindow.swift
@@ -2,14 +2,25 @@ import Cocoa
 import FlutterMacOS
 
 class MainFlutterWindow: NSWindow {
-  override func awakeFromNib() {
-    let flutterViewController = FlutterViewController.init()
-    let windowFrame = self.frame
-    self.contentViewController = flutterViewController
-    self.setFrame(windowFrame, display: true)
+    override func awakeFromNib() {
+      let flutterViewController = FlutterViewController.init()
+      self.contentViewController = flutterViewController
 
-    RegisterGeneratedPlugins(registry: flutterViewController)
+      let windowWidth: CGFloat = 375 // 원하는 너비를 설정하세요.
+      let windowHeight: CGFloat = 812 // 원하는 높이를 설정하세요.
 
-    super.awakeFromNib()
-  }
+      let screenSize = NSScreen.main?.frame.size ?? CGSize(width: 800, height: 600)
+      let screenWidth = screenSize.width
+      let screenHeight = screenSize.height
+
+      let horizontalPosition = (screenWidth - windowWidth) / 2
+      let verticalPosition = (screenHeight - windowHeight) / 2
+
+      let windowFrame = CGRect(x: horizontalPosition, y: verticalPosition, width: windowWidth, height: windowHeight)
+      self.setFrame(windowFrame, display: true)
+
+      RegisterGeneratedPlugins(registry: flutterViewController)
+
+      super.awakeFromNib()
+    }
 }
